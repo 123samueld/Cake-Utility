b955029e2bda17506c22660d5cfca1e44d3b5ec4
Samuel Davidson <123samueld@gmail.com>
2022-01-15 18:18:28 +0000

    Plan outline of script

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 include "includes/autoloader.inc.php";
 
 /*
-hello = new hello_func();
-echo hello->hello_func();
+Planning Skeleton:
+    open and read files
+    split each employee's data and assign to name and birthday variables
+    iterate through each day of the year:
+        match birthdays to dates
+            check calendar for working days (not weekends, christmas, boxing day or new years day)
+            if birthday is not working day 
+                add 1 to cakeday until date matches working day
+                create dates with names
+                if 3 cakedays in a row
+                    push back 3rd cakeday by 1 day
+                    if 2 cakeday in a row
+                        no cake first day, large cake second day
+                    if more than one name on same date
+                        provide big cake for named employees on that date
+            provide cupcake for named employee on that date
+    update the SVG file with cakeday's, num 'o cupcakes, num 'o large cakes, cake recipient names
 */
-echo "Blah blah";
-
-lalala
